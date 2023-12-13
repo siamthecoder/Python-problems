cf19f20de8648da4382302f6b3cc0e1359cffe4f
Siam Hossain <101193441+siamthecoder@users.noreply.github.com>
2023-12-14 01:38:24 +0600

    Python

---
+++ Turtle programs/triangle.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.shape("arrow")
+
+turtle.speed(2)
+
+turtle.forward(100)
+turtle.left(60)
+turtle.right(60)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
