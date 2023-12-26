7882b1469323c8a2c7ec47a0e8d64ed80c5476c7
Siam Hossain <101193441+siamthecoder@users.noreply.github.com>
2023-12-26 09:47:44 +0600

    Python

---
+++ Function/consecutiveDefault.py
@@ -0,0 +1,11 @@
+
+def myfun(x, y = 10, z = 20):
+    print("x = ", x, "y = ", y, "z = ", z)
+    
+x = 50
+y = 60
+z = 70
+
+myfun(x, y, z)
+myfun(x, y)
+myfun(x)
---
+++ Function/sendingListInParameter.py
@@ -0,0 +1,10 @@
+
+def myfunList(numbers):
+    sum = 0
+    for i in numbers:
+        sum += i;
+    return sum
+    
+    
+result = myfunList([2, 3, 4, 5, 6])
+print(result)
